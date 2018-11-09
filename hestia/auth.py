1e8cda64905919c2678fdafaccf994189433a87a
mouradmourafiq <mouradmourafiq@gmail.com>
2018-11-09 18:52:56 +0100

    Update auth

--- hestia/auth.py
+++ hestia/auth.py
@@ -2,3 +2,5 @@ class AuthenticationTypes(object):
     TOKEN = 'Token'
     INTERNAL_TOKEN = 'Internaltoken'
     EPHEMERAL_TOKEN = 'EphemeralToken'
+
+    VALUES = {TOKEN, INTERNAL_TOKEN, EPHEMERAL_TOKEN}
