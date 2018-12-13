31d4f91db8adbb2d592a02c632baaa824be9ed9d
mouradmourafiq <mouradmourafiq@gmail.com>
2018-12-13 17:04:56 +0100

    Bump version to 0.1.6

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ class PyTest(TestCommand):
 
 
 setup(name='hestia',
-      version='0.1.5',
+      version='0.1.6',
       description='Polyaxon common utilities.',
       maintainer='Mourad Mourafiq',
       maintainer_email='mourad@polyaxon.com',
