746ef337edbe0fb63efa5322fed4e5f8f8e7dc5f
mouradmourafiq <mouradmourafiq@gmail.com>
2019-02-17 18:07:12 +0100

    Add new internal service

--- hestia/internal_services.py
+++ hestia/internal_services.py
@@ -1,11 +1,13 @@
 class InternalServices(object):  # noqa
     DOCKERIZER = 'dockerizer'
+    INITIALIZER = 'initializer'
     SIDECAR = 'sidecar'
     HELPER = 'helper'
     RUNNER = 'runner'
 
     VALUES = [
         DOCKERIZER,
+        INITIALIZER,
         SIDECAR,
         HELPER,
         RUNNER
